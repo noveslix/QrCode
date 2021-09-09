8c530ebe29cededd1b4acdf3560a885b6cc5b8cc
noveslix <52439756+noveslix@users.noreply.github.com>
2021-09-09 18:45:22 +0700

    Tester

---
+++ storage.py
@@ -0,0 +1,8 @@
+scholar_dict = {
+    # 'DiscordID' : ['DiscordName', 'RoninWalletAddress', 'RoninPrivateKey']
+    '840995797025423361': [
+        'Scholars1 | DLT', '0xf14c39da55cdd8c183b44515e429f1cae00e541e',
+        '341e1de131b4f1a0c4f075ba0217b8dc126294ab3d93c008abcae5659da1c751'
+    ]
+# Put Your Discord Bot Token Here
+discord_bot_token = 'ODg1MjI4MTk1MTYxNDQwMjk2.YTj_AA.28S7P_EzehbXzRwltljW1D9MG8M'
